8d50e3c67f80cbbf93d7f0f7c9e5668661743582
Varsha Nagadwala <varshakhatri38@gmail.com>
2024-04-06 11:27:50 +0530

    Branch B Change 6 and 7 Line

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
 	
 	public static void main(String[] args) {
 		System.out.println("In Test2 File");
+		System.out.println("In Test2 Branch B line 6 change");
+		System.out.println("In Test2 Branch B line 7 change");	
 	}
 }
